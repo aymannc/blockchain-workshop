b39aacdce900e6d9681f9c894407b4569c21c283
Ayman NAIT CHERIF <ayman.naitcherif@adria-bt.com>
2023-05-10 17:03:21 +0100

    initial setup

---
+++ src/main/java/com/blockchain/blockchainworkshop/BlockchainWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.blockchain.blockchainworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blockchain/blockchainworkshop/BlockchainWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockchain.blockchainworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
